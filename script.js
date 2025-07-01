e1a3b2de8a0a35bf1a8ef140d8c1152cfea7bcec
Guilen1 <145458456+Guilen1@users.noreply.github.com>
2025-07-01 11:36:55 -0300

    aula 01-07

---
+++ script.js
@@ -0,0 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [ 
+    {
+        enunciado:a primeira perguntas
+        alternativas: [
+            "isso e ruim",
+            "isso e bom"
+        ]
+    },
+    {
+        enunciado:a primeira perguntas
+        alternativas: [
+            "isso e ruim",
+            "isso e bom"
+        ]
+    },
+    {
+        enunciado:a primeira perguntas
+        alternativas: [
+            "isso e ruim",
+            "isso e bom"
+        ]
+    },
+    {
+        enunciado:a primeira perguntas
+        alternativas: [
+            "isso e ruim",
+            "isso e bom"
+        ]
+    },
+    {
+        enunciado:a primeira perguntas
+        alternativas: [
+            "isso e ruim",
+            "isso e bom"
+        ]
+    },
+]
