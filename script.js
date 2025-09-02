29b4a995e878298bdbe44851a964deca75c6d8f4
Guilen1 <145458456+Guilen1@users.noreply.github.com>
2025-09-02 11:15:33 -0300

    att

--- script.js
+++ script.js
@@ -45,28 +45,28 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Um colega comenta que ",
+        enunciado: "Um colega comenta que usou IA para revisar o texto da redaçâo dele.",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Apoiou a ideia, pois a IA pode ajudar a corrigir erros de gramática.",
+                afirmacao:  "Reconheceu a IA como uma ferramenta de apoio no aprendizado."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Criticou, dizendo que o colega deveria revisar sozinho.",
+                afirmacao: "Defendeu a importância do esforço próprio no estudo."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Você descobre um aplicativo de tradução automática baseado em IA. ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Achou interessante, pois pode ajudar a se comunicar com pessoas de outros países.",
+                afirmacao: "Percebeu como a IA aproxima culturas diferente."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Ficou desconfiado, achando que as traduções não seriam confiáveis.",
+                afirmacao: "Manteve uma postura crítica em relação ao uso da IA. "
             }
         ]
     },
